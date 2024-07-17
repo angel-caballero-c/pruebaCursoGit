81d469670424f96237328d0ee44076d4b7397f72
Angel_Caballero <caballerocedillo.la@gmail.com>
2024-07-17 14:23:57 -0600

    Dos archivos distintos, uno con suma y resta, el otro multiplicación y división

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+#Calculadora
+
+a = int(input("introduce el numero 1: "))
+
+b = int(input("introduce el numero 2: "))
+
+sumar = a + b
+
+restar = a - b
+
+
+print (sumar, restar)
+
+
+
+def suma(a, b): 
+    
+    return a + b
---
+++ nuevaCalculadora.py
@@ -0,0 +1,14 @@
+a = int(input("introduce el numero 1:" ))
+
+b = int(input("introduce el numero 2:" ))
+
+dividir = a / b
+multiplicar = a * b
+
+print("La division es: ", dividir)
+
+print("El producto es: ", multiplicar)
+
+if b != 0:
+    resto = a % b
+    print("El resto es: ", resto)
